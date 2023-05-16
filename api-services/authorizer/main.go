d2c1511792dd34ad53b050ae3bd7fbf9367ff50e
yuanjizhai <yuanji.zhai@outlook.com>
2023-05-16 23:58:43 +0800

    update custom authorizer

--- api-services/authorizer/main.go
+++ api-services/authorizer/main.go
@@ -6,8 +6,13 @@ package main
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
 )
 
 func main() {
@@ -15,12 +20,38 @@ func main() {
 }
 
 func handleRequest(_ context.Context, event events.APIGatewayV2CustomAuthorizerV2Request) (events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
-
-	authResponse := events.APIGatewayV2CustomAuthorizerSimpleResponse{IsAuthorized: true}
+	//- $request.header.X-Api-Key
+	//- $request.header.X-App-Name
+	headers := event.Headers
+	key := headers["X-Api-Key"]
+	appName := headers["X-App-Name"]
+	fmt.Println("X-Api-Key", key, "X-App-Name", appName)
+	ok := verifyApiKey(appName, key)
+	authResponse := events.APIGatewayV2CustomAuthorizerSimpleResponse{IsAuthorized: ok}
 	authResponse.Context = map[string]interface{}{
-		"stringKey":  "serverless authorizer",
+		"stringKey":  "serverless custom authorizer",
 		"numberKey":  2023,
 		"booleanKey": true,
 	}
 	return authResponse, nil
+}
+
+func verifyApiKey(appName string, apiKey string) bool {
+	sign, err := calcApiKey(appName)
+	if err != nil {
+		return false
+	}
+	return sign == apiKey
+}
+
+func calcApiKey(appName string) (string, error) {
+	salt := os.Getenv("salt")
+	if len(salt) == 0 {
+		return "", errors.New("salt is empty")
+	}
+	h := md5.New()
+	h.Write([]byte(appName))
+	h.Write([]byte(salt))
+	s := h.Sum(nil)
+	return hex.EncodeToString(s), nil
 }
